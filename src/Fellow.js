ce1d612853ab0a67dc050a145b4691fc77a63eee
Deepak Agrawal <debug.career@gmail.com>
2021-02-08 00:10:16 +0530

    Profile photos added

--- src/Fellow.js
+++ src/Fellow.js
@@ -9,17 +9,22 @@ class Fellow extends React.Component {
   render(props) {
     return (
       <div>
-          <div id="card">
-            <img alt ="background" src="https://th.bing.com/th/id/OIP.Cvogu9sG2nhb9_yBPIMedAHaHa?pid=Api&rs=1"/>
-            <h2>{this.props.dataprop.name} {this.props.dataprop.ID}</h2>
-            <p>{this.props.dataprop.kind}</p>
-            <p>
+        <div id="card">
+          <img alt="profilePicture" src={this.props.dataprop.profile} />
+          <h2>{this.props.dataprop.name} {this.props.dataprop.ID}</h2>
+          <p>{this.props.dataprop.kind}</p>
+          <p>
             {this.props.dataprop.tech}
-            </p>
-            <p>Love Hackathons and like helping Helping Others ;)</p>
-            <span className="right bottom"><FaGithubSquare size={25} color="#333"/>  <FaLinkedin size={25} color="#333"/> <FaTwitterSquare size={25} color="#333"/>  <FaFacebookSquare size={25} color="#333"/></span>
-          </div>
+          </p>
+          <p>Love Hackathons and like helping Helping Others ;)</p>
+          <span className="right bottom">
+          <a href={`https://github.com/${this.props.dataprop.github_username}`}> <FaGithubSquare size={25} color="#333" /></a>
+          <a href={`https://linkedin.com/${this.props.dataprop.linkedin_username}`}> <FaLinkedin size={25} color="#333" /></a> 
+          <a href={`https://twitter.com/${this.props.dataprop.twitter_username}`}> <FaTwitterSquare size={25} color="#333" /></a>  
+          <a href={`https://facebook.com/${this.props.dataprop.facebook_username}`}> <FaFacebookSquare size={25} color="#333" /></a>
+          </span>
         </div>
+      </div>
     );
   }
 }
